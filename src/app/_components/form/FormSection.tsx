5be4bc1cc97ee5e2d25febc3f679f27beb37bc87
Tomasz Ceremuga <tomaszmceremuga@gmail.com>
2025-03-06 14:38:59 +0100

    form section fix

--- src/app/_components/form/FormSection.tsx
+++ src/app/_components/form/FormSection.tsx
@@ -8,21 +8,28 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
+import SelectGroup from "./SelectGroup";
+import SelectMultipleGroup from "./SelectMultipleGroup";
+
+import RadioElements from "./RadioElements";
+
 const FormSection = () => {
   return (
-    <div>
-      <p>Formularz</p>
-      <p> cos innego</p>
-      <Select>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Theme" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="light">Light</SelectItem>
-          <SelectItem value="dark">Dark</SelectItem>
-          <SelectItem value="system">System</SelectItem>
-        </SelectContent>
-      </Select>
+    <div className="pr-16">
+      <SelectGroup
+        name="Wybierz cosia"
+        options={["cos", "cos1", "cos2", "cos3"]}
+      />
+      <RadioElements
+        name="Wybierz opcje"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam convallis libero in ullamcorper iaculis."
+        options={["cos", "cos1", "cos2"]}
+      />
+      <SelectMultipleGroup
+        name="Wybierz kilka cosiów"
+        options={["cos", "cos1", "cos2", "cos2"]}
+        isImportant={true}
+      />
     </div>
   );
 };
