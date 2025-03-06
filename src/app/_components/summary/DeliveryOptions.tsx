f1fa741fd96c179055dfbed792b40595ce3740ec
adamjasiewicz <adamjasiewicz425@gmail.com>
2025-03-06 13:09:19 +0100

    Update summary section

---
+++ src/app/_components/summary/DeliveryOptions.tsx
@@ -0,0 +1,25 @@
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+
+const DeliveryOptions = () => {
+  return (
+    <div>
+      <RadioGroup defaultValue="option-one">
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem value="option-one" id="option-one" />
+          <Label htmlFor="option-one">Option One</Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem value="option-two" id="option-two" />
+          <Label htmlFor="option-two">Option Two</Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem value="option-two" id="option-two" />
+          <Label htmlFor="option-two">Option Three</Label>
+        </div>
+      </RadioGroup>
+    </div>
+  );
+};
+
+export default DeliveryOptions;
--- src/app/_components/summary/SummarySection.tsx
+++ src/app/_components/summary/SummarySection.tsx
@@ -1,10 +1,29 @@
 import React from "react";
+import DeliveryOptions from "./DeliveryOptions";
+import SummaryButtons from "./SummaryButtons";
 
 const SummarySection = () => {
   return (
-    <div>
-      <p>Podsumowanie</p>
-      <p>To jest sekcja Adasia</p>
+    <div className="flex items-start justify-between gap-8">
+      <div className="w-1/2">
+        <p>Opcję Dostawy</p>
+        <DeliveryOptions />
+      </div>
+      <div className="w-1/2">
+        <p>
+          CENA: xxx ZŁ
+          <sub>
+            <span>(ilość elementów)</span>
+          </sub>
+        </p>
+        <p>
+          <sup>
+            <span>*</span>
+          </sup>
+          W cenę wliczony jest podatek VAT
+        </p>
+        <SummaryButtons />
+      </div>
     </div>
   );
 };
