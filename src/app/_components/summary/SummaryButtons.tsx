dad2a9ab43ab85eb80e9e963c6731b4ee492460e
Tomasz Ceremuga <tomaszmceremuga@gmail.com>
2025-03-07 08:24:33 +0100

    summary buttons fix

--- src/app/_components/summary/SummaryButtons.tsx
+++ src/app/_components/summary/SummaryButtons.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 const SummaryButtons = () => {
   return (
     <div className="flex flex-col gap-2">
-      <Button variant="outline">DODAJ KOLEJNY ELEMENT</Button>
+      <Button variant="default">DODAJ KOLEJNY ELEMENT</Button>
       <Button variant="default">PRZEJDŹ DO PŁATNOŚCI</Button>
     </div>
   );
