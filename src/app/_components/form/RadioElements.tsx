d31f24b6f9f1a190c8fb3bf8e4bc47414678d0d3
Tomasz Ceremuga <tomaszmceremuga@gmail.com>
2025-03-07 08:21:43 +0100

    form elements changes

--- src/app/_components/form/RadioElements.tsx
+++ src/app/_components/form/RadioElements.tsx
@@ -2,28 +2,32 @@ import React from "react";
 
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import InfoTooltip from "./InfoTooltip";
 
 interface RadioElementsProps {
   name: string;
-  description: string;
+  description?: string;
   options: string[];
   isImportant?: boolean;
 }
 
 const RadioElements: React.FC<RadioElementsProps> = ({
   name,
-  description,
+  description = "",
   options,
   isImportant = false,
 }) => {
   return (
     <div className="mb-5 p-2">
+      <InfoTooltip>pomoc tu jest</InfoTooltip>
       <p className="whitespace-nowrap p-[6px] text-base">
         {isImportant && <span className="mr-1 text-red-500">*</span>}
         {name}
       </p>
-      <p className="ml-5 pb-4 text-neutral-500">{description}</p>
-      <RadioGroup className="ml-5 flex" defaultValue="option-0">
+      {description != "" && (
+        <p className="ml-5 pb-1 text-neutral-500">{description}</p>
+      )}
+      <RadioGroup className="ml-5 flex pt-2" defaultValue="option-0">
         {options.map((option, index) => (
           <div key={index} className="mr-3 flex items-center space-x-2">
             <RadioGroupItem value={`option-${index}`} id={`option-${index}`} />
--- src/app/_components/form/SelectGroup.tsx
+++ src/app/_components/form/SelectGroup.tsx
@@ -1,38 +1,3 @@
-// import React from "react";
-
-// interface SelectGroupProps {
-//   name: string;
-//   options: string[];
-//   isImportant?: boolean;
-// }
-
-// const SelectGroup: React.FC<SelectGroupProps> = ({
-//   name,
-//   options,
-//   isImportant = false,
-// }) => {
-//   return (
-//     <div className="mb-10 flex flex-wrap items-center">
-//       <div className="ml-2 flex flex-wrap gap-2">
-//         <p className="whitespace-nowrap p-[6px] text-base">
-//           {isImportant && <span className="mr-1 text-red-500">*</span>}
-//           {name}
-//         </p>
-//         {options.map((option, index) => (
-//           <button
-//             key={index}
-//             className="ring-offset-background hover:bg-muted hover:text-muted-foreground focus-visible:ring-ring data-[state=on]:bg-accent data-[state=on]:text-accent-foreground inline-flex h-9 min-w-9 items-center justify-center gap-2 rounded-md border px-2.5 text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
-//           >
-//             {option}
-//           </button>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default SelectGroup;
-
 "use client";
 import React, { useState } from "react";
 
@@ -50,7 +15,7 @@ const SelectGroup: React.FC<SelectGroupProps> = ({
   const [selectedOption, setSelectedOption] = useState<string | null>(null);
 
   const handleClick = (option: string) => {
-    setSelectedOption(option);
+    setSelectedOption((prev) => (prev === option ? null : option));
   };
 
   return (
@@ -66,9 +31,9 @@ const SelectGroup: React.FC<SelectGroupProps> = ({
             onClick={() => handleClick(option)}
             className={`${
               selectedOption === option
-                ? "bg-accent text-accent-foreground"
+                ? "bg-accent-foreground text-accent"
                 : "hover:bg-muted hover:text-muted-foreground"
-            } ring-offset-background inline-flex h-9 min-w-9 items-center justify-center gap-2 rounded-md border px-2.5 text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50`}
+            } inline-flex h-9 min-w-9 items-center justify-center gap-2 rounded-md border px-2.5 text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50`}
           >
             {option}
           </button>
