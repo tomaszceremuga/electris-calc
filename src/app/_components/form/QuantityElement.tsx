dc3fe3641e90b73e502d2f398e6eac3bb8f0b225
Tomasz Ceremuga <tomaszmceremuga@gmail.com>
2025-03-10 09:03:31 +0100

    hydration problem in QuantityElement fix

--- src/app/_components/form/QuantityElement.tsx
+++ src/app/_components/form/QuantityElement.tsx
@@ -37,9 +37,9 @@ const QuantityElement: React.FC<QuantityElementProps> = ({
         </div>
         <Popover>
           <PopoverTrigger>
-            <Button variant={"outline"} className="w-32">
+            <div className="inline-flex h-10 w-32 items-center justify-center gap-2 whitespace-nowrap rounded-md border border-input bg-background px-4 py-2 text-sm font-medium ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0">
               {quantity == 0 ? "" : quantity}
-            </Button>{" "}
+            </div>{" "}
           </PopoverTrigger>
           <PopoverContent className="w-full max-w-[700px]">
             <QuantityTable setQuantity={setQuantity} />
