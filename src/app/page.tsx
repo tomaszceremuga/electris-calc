0fa54e81fbf0e6ef0984b0e4f12d97d14c70c15a
Tomasz Ceremuga <tomaszmceremuga@gmail.com>
2025-03-14 15:17:38 +0100

    page fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-import Link from "next/link";
+// import Link from "next/link";
 
-import { api, HydrateClient } from "~/trpc/server";
+// import { api, HydrateClient } from "~/trpc/server";
+
+import { HydrateClient } from "~/trpc/server";
 
 import FloatingCart from "./_components/FloatingCart";
 import Navbar from "./_components/Navbar";
@@ -10,7 +12,7 @@ export default async function Home() {
   return (
     <HydrateClient>
       <Navbar />
-      <div className="flex flex-wrap lg:flex-nowrap lg:min-h-screen justify-center lg:gap-x-5  lg:bg-gray-50 p-4 pt-10">
+      <div className="flex flex-wrap justify-center p-4 pt-10 lg:min-h-screen lg:flex-nowrap lg:gap-x-5 lg:bg-gray-50">
         <AccordionWithNavigation />
         <FloatingCart />
       </div>
