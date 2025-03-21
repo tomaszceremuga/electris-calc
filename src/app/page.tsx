ec5394e38ba1d748c802b0ccbb492eda138fbecd
adamjasiewicz <adamjasiewicz425@gmail.com>
2025-03-21 14:55:19 +0100

    Router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,23 +5,33 @@
 import { HydrateClient } from "~/trpc/server";
 
 import Navbar from "./_components/Navbar";
-import AccordionWithNavigation from "./_components/AccordionWithNavigation";
-import CustomCart from "./_components/cart/CustomCart";
-import { CartProvider } from "~/lib/CartContext";
-import { FormProvider } from "~/lib/FormContext";
+
+import Image from "next/image";
+import { Button } from "~/components/ui/button";
 
 export default async function Home() {
   return (
     <HydrateClient>
       <Navbar />
 
-      <div className="flex flex-wrap justify-center p-4 pt-10 lg:min-h-screen lg:flex-nowrap lg:gap-x-5 lg:bg-gray-50">
-        <FormProvider>
-          <CartProvider>
-            <AccordionWithNavigation />
-            <CustomCart />
-          </CartProvider>
-        </FormProvider>
+      <div className="relative min-h-[calc(100vh-4rem)] w-full">
+        <Image
+          src="/LandingPage.png"
+          alt="Landing Page"
+          fill
+          priority
+          sizes="100vw"
+          className="object-cover"
+        />
+        <div className="absolute inset-0 flex flex-col items-center justify-center bg-black/50 px-4 text-center text-white">
+          <h1 className="text-2xl font-bold md:text-4xl">Zapytaj o wycenę</h1>
+          <p className="mt-2 text-lg md:text-xl">
+            Chcesz nawiązać z nami współpracę?
+          </p>
+          <Button variant="default" className="mt-4 px-6 py-3 text-lg">
+            KLIK
+          </Button>
+        </div>
       </div>
     </HydrateClient>
   );
---
+++ src/app/zamowienie/layout.tsx
@@ -0,0 +1,25 @@
+import "~/styles/globals.css";
+import { Toaster } from "@/components/ui/sonner";
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+
+import { TRPCReactProvider } from "~/trpc/react";
+
+export const metadata: Metadata = {
+  title: "Wyfrezuj to",
+  description: "Złóż zamóweinie na blachy!",
+  icons: [{ rel: "icon", url: "/electris.png" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${GeistSans.variable}`}>
+      <body>
+        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <Toaster className="" />
+      </body>
+    </html>
+  );
+}
